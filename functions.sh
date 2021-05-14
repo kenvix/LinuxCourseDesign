8d7157abc30c8d716b3b5c5c6ce4a01f73a0a107
Kenvix Zure <kenvixzure@live.com>
2021-05-14 22:22:26 +0800

    Refactor preparing

--- functions.sh
+++ functions.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 
-
 export SQL_FILE=${SQL_FILE-"main.sqlite3"}
 export VERSION=3
 ALTERNATIVE_LOG_OUTPUT_DESCRIPTOR=0
 
 function checkParamNum {
-    if (($# < 1)); then 
+    if (($# < 1)); then
         logE "Illegal checkParamNum call, at least one param required."
         return 2
     fi
-    
+
     local minParamNum=$1
     shift
     if (($# < $minParamNum)); then
@@ -91,7 +90,7 @@ function sendFile {
 
 function sendCSV {
     logD "[SEND-CSV]: $1"
-    local csv=`cat "$1"`
+    local csv=$(cat "$1")
     echo -e -n "${csv//[,]/\t}" >&4
 }
 
@@ -113,7 +112,7 @@ function sqlAdds {
 
 function getUserIdByStudentId {
     checkParamNum 1 $*
-    local studentId=`sqlAdds "$1"`
+    local studentId=$(sqlAdds "$1")
     execSQLRO "SELECT userid FROM users WHERE studentid = '$studentId';"
 }
 
@@ -129,27 +128,28 @@ function addElementOrIncKillNumByTypeName {
 
     subtype="${subtype//[-]/}"
     sku="${sku//[-]/}"
-    
+
     local existOne=$(execSQLRO "SELECT date,killnum,id FROM elements WHERE x = '$x' AND y = '$y'")
-    IFS='|' read -r -a existOne <<< "$existOne"
+    IFS='|' read -r -a existOne <<<"$existOne"
+
+    local typeid=$(execSQLRO "SELECT typeid FROM types WHERE types.type LIKE '$type' AND types.subtype LIKE '$subtype' AND types.sku LIKE '$sku'")
+    if [[ "$typeid" == "" ]]; then
+        logW "无法找到这样的类型，请核查：$type $subtype $sku"
+        return 3
+    fi
+
     if ((${#existOne[@]} == 3)); then
         if [[ "${existOne[0]}" == "$today" ]]; then
             log "检测到同日重复，将忽略: $*"
             return 2
         else
             logD "更新已存在的数据: ${existOne}"
-            execSQL "UPDATE elements SET date = '$today', killnum = ${existOne[1]} + 1 WHERE id = ${existOne[2]};"
+            execSQL "UPDATE elements SET typeid = '$typeid', date = '$today', killnum = ${existOne[1]} + 1 WHERE id = ${existOne[2]};"
         fi
     else
 
-        local typeid=`execSQLRO "SELECT typeid FROM types WHERE types.type LIKE '$type' AND types.subtype LIKE '$subtype' AND types.sku LIKE '$sku'"`
-        if [[ "$typeid" == "" ]]; then
-            logW "无法找到这样的类型，请核查：$type $subtype $sku"
-            return 3
-        fi
-
         logD "TypeID: $typeid"
-        execSQL "INSERT INTO elements (userid, typeid, x, y, date) VALUES ( '$userid', '$typeid', '$x', '$y', DATE() )"  
+        execSQL "INSERT INTO elements (userid, typeid, x, y, date) VALUES ( '$userid', '$typeid', '$x', '$y', DATE() )"
     fi
 }
 
@@ -177,7 +177,7 @@ function exportDailyRank {
     local date="${1-"$(getDate)"}"
     local fileName="${2-"stat-$(getDate).html"}"
     log "导出 $date 的数据，并存储到：$fileName"
-    
+
     exec 9<>"$fileName"
     echo '<!DOCTYPE html>
 <html>
@@ -186,17 +186,17 @@ function exportDailyRank {
 	<meta http-equiv="charset" content="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-    <style>table,table tr th, table tr td { border:1px solid #0094ff; }</style>'  >&9
+    <style>table,table tr th, table tr td { border:1px solid #0094ff; }</style>' >&9
 
     echo "<title>塞尔达传说 $(getDate) 的所有数据和排行榜</title>" >&9
-    echo "</head><body>" >&9    
-    echo "<h1>可视化地图</h1>" >&9 
-    echo '<canvas id="cv" width="98%" height="98vh" style="border:1px solid #000000;"></canvas>' >&9 
-    echo '<script>var c = document.getElementById("cv"); var ctx = c.getContext("2d");' >&9 
+    echo "</head><body>" >&9
+    echo "<h1>可视化地图</h1>" >&9
+    echo '<canvas id="cv" width="98%" height="98vh" style="border:1px solid #000000;"></canvas>' >&9
+    echo '<script>var c = document.getElementById("cv"); var ctx = c.getContext("2d");' >&9
     echo '
   c.width  = window.innerWidth - 40;
-  c.height = window.innerHeight - 40;'  >&9 
-    
+  c.height = window.innerHeight - 40;' >&9
+
     local elements=$(execSQLRO "SELECT type, subtype, x, y, (
 	CASE
 		WHEN subtype IS NULL OR subtype = '' THEN type
@@ -221,14 +221,14 @@ function exportDailyRank {
 
     echo "$elements" | {
         while IFS= read -t 0.1 -r line; do
-            IFS='|' read -r -a line <<< "$line"
+            IFS='|' read -r -a line <<<"$line"
             echo "ctx.fillText('${line[5]}', ${line[2]}, ${line[3]});" >&9
         done
-    } 
+    }
 
-    echo '</script>' >&9 
+    echo '</script>' >&9
 
-    echo "<h1>目前的全部数据</h1>" >&9 
+    echo "<h1>目前的全部数据</h1>" >&9
 
     echo "<table>" >&9
     echo "<tr> <th>ID</th> <th>类型</th> <th>子类</th> <th>特点</th> <th>X</th>  <th>Y</th> <th>击杀次数</th> <th>提交人姓名</th> <th>学号</th> <th>时间</th> </tr>" >&9
@@ -236,81 +236,73 @@ function exportDailyRank {
     execSQLRO '.mode html' \
         '.output stdout' \
         'SELECT `id`, `type`, `subtype`, `sku`, `x`, `y`,`killnum` , `name`, `studentid`, `date` FROM everything ORDER BY datetime(`date`) DESC;' >&9
-    
+
     echo "</table>" >&9
 
-
-    echo "<h1>$date 的排行榜</h1>" >&9 
+    echo "<h1>$date 的排行榜</h1>" >&9
 
     echo "<h2>神庙高手排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th> <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND type LIKE '神庙' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-    
-
     echo "<h2>岩石巨人杀手排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th>  <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND subtype LIKE '岩石巨人' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-
     echo "<h2>西诺克斯杀手排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th>  <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND subtype LIKE '独眼巨人西诺克斯' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-
     echo "<h2>莱尼尔杀手排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th>  <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND subtype LIKE '半人马莱尼尔' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-
     echo "<h2>莫尔德拉吉克杀手排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th>  <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND subtype LIKE '莫尔德拉吉克' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-
     echo "<h2>克洛格排行榜</h2>" >&9
     echo "<table>" >&9
     echo "<tr>  <th>姓名</th>  <th>学号</th>  <th>类目数</th> <th>击杀总数</th></tr>" >&9
-    
+
     execSQLRO ".mode html" \
         ".output stdout" \
         "SELECT name, studentid, subtype_count, kill_count FROM daily_rank_by_type WHERE date LIKE '$date' AND type LIKE '克洛格种子' LIMIT 10;" >&9
 
     echo "</table>" >&9
 
-
     echo "</body></html>" >&9
-    exec 9>&-   
+    exec 9>&-
 }
 
 function connectAndSendCSV {
@@ -323,14 +315,14 @@ function connectAndSendCSV {
     if (($? != 0)); then
         logE "连接服务器失败"
         return 2
-    fi 
+    fi
 
     log "连接成功"
 
-    sendHeader  || return 1
+    sendHeader || return 1
     sendType "add" || return 1
     sendLine "$studentId" || return 1
-    sendLine "$(wc -l <  "$file")" || return 1
+    sendLine "$(wc -l <"$file")" || return 1
     sendCSV "$file" || return 1
     sendFooter || return 1
 
